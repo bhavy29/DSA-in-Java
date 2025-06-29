4b467fe532ba6174f79718e3863b467c4524d636
Bhavy Chaudhary <161184145+bhavy29@users.noreply.github.com>
2025-06-29 09:38:45 +0530

    Maximum Sum Subarray of Size K

---
+++ Arrays/17_Maximum_Sum_Subarray_of_Size_K.java
@@ -0,0 +1,30 @@
+// Maximum Sum Subarray of Size K
+/* Problem Statement:
+      Given an array Arr of size N and an integer K, return the maximum sum of any contiguous subarray of size K.  */
+
+public class Solution{
+  public int maxSum(int[] nums, int k){
+    int n = nums.length;
+    int max = 0;
+    int sum = 0;
+    int i = 0;
+    int j = 0;
+
+    // create window 
+    while(j<k){
+      sum = sum + nums[j];
+      j++;
+    }
+    max = sum;
+
+    // sliding window
+    while(j<n){
+      sum = sum - nums[i] + nums[j];
+      max = Math.max(max,sum);
+      i++;
+      j++;
+    }
+    return max;
+  }
+}
+  
