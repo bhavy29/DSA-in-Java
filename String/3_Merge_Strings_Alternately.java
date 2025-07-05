451777f06301aa1d531687c670988c524d4b9c1f
Bhavy Chaudhary <161184145+bhavy29@users.noreply.github.com>
2025-07-05 08:19:28 +0530

    3_Merge_Strings_Alternately.java

--- String/3_Merge_Strings_Alternately.java
+++ String/3_Merge_Strings_Alternately.java
@@ -2,28 +2,38 @@
 
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        StringBuilder ans = new StringBuilder();
+        String ans = new String();
 
-        int i = 0, j = 0;
         int n1 = word1.length();
         int n2 = word2.length();
+        int turn = 0;
 
-        while (i < n1 && j < n2) {
-            ans.append(word1.charAt(i++));
-            ans.append(word2.charAt(j++));
+        int i = 0;
+        int j = 0;
+        while(i<n1 && j<n2){
+            if(turn == 0){
+                ans = ans + word1.charAt(i);
+                i++;
+                turn = 1;
+            }
+            else if(turn == 1){
+                ans = ans + word2.charAt(j);
+                j++;
+                turn = 0;
+            }
         }
-
-        while (i < n1) {
-            ans.append(word1.charAt(i++));
+        while(i<n1){
+            ans = ans + word1.charAt(i);
+            i++;
         }
-
-        while (j < n2) {
-            ans.append(word2.charAt(j++));
+        while(j<n2){
+            ans = ans + word2.charAt(j);
+            j++;
         }
-
-        return ans.toString();
+        return ans;
     }
 }
+
 
 //----------------------------OR-------------------------
 
