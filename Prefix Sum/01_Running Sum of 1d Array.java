d4a32a198eeb3f4cd417fd6a50f28e2ecc59f611
Bhavy Chaudhary <161184145+bhavy29@users.noreply.github.com>
2026-01-08 16:28:14 +0530

    Refactor running sum calculation in Java

--- Prefix Sum/01_Running Sum of 1d Array.java
+++ Prefix Sum/01_Running Sum of 1d Array.java
@@ -3,13 +3,24 @@
 class Solution {
     public int[] runningSum(int[] nums) {
         int n = nums.length;
-        int[] runningSum = new int[n];
-        int sum = 0;
         
-        for(int i=0; i<n; i++){
-            runningSum[i] = sum + nums[i];
-            sum = runningSum[i];
+        for(int i=1; i<n; i++){
+            nums[i] += nums[i-1];
         }
-        return runningSum;
+        return nums;
     }
 }
+
+// class Solution {
+//     public int[] runningSum(int[] nums) {
+//         int n = nums.length;
+//         int[] runningSum = new int[n];
+//         int sum = 0;
+        
+//         for(int i=0; i<n; i++){
+//             runningSum[i] = sum + nums[i];
+//             sum = runningSum[i];
+//         }
+//         return runningSum;
+//     }
+// }
