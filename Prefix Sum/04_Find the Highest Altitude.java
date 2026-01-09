1a7e8e77abda707ff0868791b42aafea968d0e19
Bhavy Chaudhary <161184145+bhavy29@users.noreply.github.com>
2026-01-09 16:11:47 +0530

    Create 04_Find the Highest Altitude.java

---
+++ Prefix Sum/04_Find the Highest Altitude.java
@@ -0,0 +1,12 @@
+// LC 1732. Find the Highest Altitude
+
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int max = Math.max(0,gain[0]);
+        for(int i=1; i<gain.length; i++){
+            gain[i] += gain[i-1];
+            max = Math.max(max,gain[i]);
+        }
+        return max;
+    }
+}
